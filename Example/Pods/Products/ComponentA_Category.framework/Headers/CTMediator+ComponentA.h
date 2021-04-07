faf29326c20aa2642717f73e6fe28abe58194314
王俣 <861573477@qq.com>
2021-04-07 17:59:48 +0800

    增加依赖

---
+++ Example/Pods/Products/ComponentA_Category.framework/Headers/CTMediator+ComponentA.h
@@ -0,0 +1,16 @@
+//
+//  CTMediator+ComponentA.h
+//  ComponentA_Category
+//
+//  Created by 王俣2020 on 2021/4/6.
+//
+
+#import <CTMediator/CTMediator.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator (ComponentA)
+
+@end
+
+NS_ASSUME_NONNULL_END
