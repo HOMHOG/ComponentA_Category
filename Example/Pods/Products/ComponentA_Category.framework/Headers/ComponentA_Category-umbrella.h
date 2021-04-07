1f9574d4d46341de542a674ce45e5312a2694635
王俣 <861573477@qq.com>
2021-04-07 15:56:24 +0800

    合并framework

---
+++ Example/Pods/Products/ComponentA_Category.framework/Headers/ComponentA_Category-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "CTMediator+ComponentA.h"
+#import "Test.h"
+
+FOUNDATION_EXPORT double ComponentA_CategoryVersionNumber;
+FOUNDATION_EXPORT const unsigned char ComponentA_CategoryVersionString[];
+
